e175e8be041dceabdd57835cfce5e395cfcf8109
Jordan-182 <jordan.p62@outlook.fr>
2024-02-05 22:11:42 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['poppins', 'sans-rerif'],
+      },
+      backgroundImage: {
+        'bgimg': "url('/src/assets/img/background.jpg')",
+    },
+  }
+  },
+  plugins: [],
+};
